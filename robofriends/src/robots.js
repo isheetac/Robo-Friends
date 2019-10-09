adeb83d7f5a185c170a50cfd15a021e1464d9f3a
Isheeta Chinchankar <isheeta.chinchankar@gmail.com>
2019-10-09 09:47:51 +0530

    added project

---
+++ robofriends/src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Mack Greene',
+        username: 'Bret',
+        email: 'mackgreen@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ruari Cresswell',
+        username: 'Bob',
+        email: 'ruari@comcast.net'
+    },
+    {
+        id: 3,
+        name: 'Aadam Patel',
+        username: 'Samantha',
+        email: 'aadamp@yahoo.com'
+    },
+    {
+        id: 4,
+        name: 'Anwen Powers',
+        username: 'Charlie',
+        email: 'powers@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Agnes Daniels',
+        username: 'Austin',
+        email: 'adaniels@outlook.com'
+    },
+    {
+        id: 6,
+        name: 'Charlie Floyd',
+        username: 'Rebecca',
+        email: 'cf@yahoo.com'
+    },
+    {
+        id: 7,
+        name: 'Anoushka Thorne',
+        username: 'Karen',
+        email: 'athorne@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Tayla Meza',
+        username: 'Christina',
+        email: 'taylameza@comcast.net'
+    },
+    {
+        id: 9,
+        name: 'Nico Tapia',
+        username: 'Paul',
+        email: 'nicot@yahoo.com'
+    },
+    {
+        id: 10,
+        name: 'Marco Ferry',
+        username: 'Roger',
+        email: 'ferrym@gmail.com'
+    },
+]
