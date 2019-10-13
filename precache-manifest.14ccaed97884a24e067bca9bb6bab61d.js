57fba465f210ad1ec6b5b0965718d87bdf0b4d88
Isheeta Chinchankar <isheeta.chinchankar@gmail.com>
2019-10-13 08:25:22 +0530

    Updates

---
+++ precache-manifest.14ccaed97884a24e067bca9bb6bab61d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f1cc1b989e82d1cbc06d59e8908d8b",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "5919f07b73c1af02a415",
+    "url": "/Robo-Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d6740b2fde1d50224b10",
+    "url": "/Robo-Friends/static/css/main.01546b02.chunk.css"
+  },
+  {
+    "revision": "5919f07b73c1af02a415",
+    "url": "/Robo-Friends/static/js/2.3c2b84a9.chunk.js"
+  },
+  {
+    "revision": "d6740b2fde1d50224b10",
+    "url": "/Robo-Friends/static/js/main.4c697208.chunk.js"
+  },
+  {
+    "revision": "2eeccaaca14c34420ddc",
+    "url": "/Robo-Friends/static/js/runtime-main.21501f20.js"
+  },
+  {
+    "revision": "add573b7dfa735ab4e4ed24dd94a40ee",
+    "url": "/Robo-Friends/static/media/Robot Reavers.add573b7.woff"
+  }
+]);
